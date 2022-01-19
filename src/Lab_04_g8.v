f111cb5390cd4ce831ae2ffc247701e2a7749bad
jubarreras <92691359+jubarreras@users.noreply.github.com>
2022-01-18 21:01:51 -0500

    Update Lab_04_g8.v

--- src/Lab_04_g8.v
+++ src/Lab_04_g8.v
@@ -1,14 +1,14 @@
-`timescale 1ns / 1ps
+
 module Lab_04_g8 (addrA, addrB, addrw, datw, Regwrite, clk, rst, SSeg, An);
 
 input [3:0] addrA;
 input [3:0] addrB;
 input [3:0] addrw;
 input [3:0] datw;
-input [3:0] Regwrite;
-input [3:0] clk;
-input [3:0] rst;
-output [6:0] SSeg;
+input Regwrite;
+input clk;
+input rst;
+output [0:6] SSeg;
 output [3:0] An;
 
 wire [3:0] datA;
